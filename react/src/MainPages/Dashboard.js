487bafb475ba6eb4268c2a6f6818a77837b4f39d
zoee-Y <44394845+zoee-Y@users.noreply.github.com>
2023-06-05 22:01:32 +0800

    slight changes to css, add current charging box

--- react/src/MainPages/Dashboard.js
+++ react/src/MainPages/Dashboard.js
@@ -11,6 +11,7 @@ export default function Dashboard() {
           style={{
             height: "75vh"
           }}>
+
           <div className="absolute top-0 w-full h-full bg-center bg-cover"
             style={{
               backgroundImage: "url('landing-image.jpg')"
@@ -34,10 +35,34 @@ export default function Dashboard() {
           </div>
         </div>
 
+
+
         <section className="bg-gray-300 -mt-24">
-          <div className="container mx-auto px-4">
+
+          {/* Charging status */}
+          <div className="container mx-auto px-4 -mt-52">
+            <div className="flex justify-center w-full">
+              <div className="w-full md:w-1/2 px-4">
+                <div className="relative flex flex-col min-w-0 break-words bg-lime-100 mb-8 shadow-lg rounded-lg">
+                  <div className="px-4 py-5 flex-auto">
+                    <h6 className="text-lg font-semibold uppercase text-center">Current charging status</h6>
+
+                    <div className="space-y-1 grid grid-cols-2 text-center">
+                      <div>Vehicle: Vehicle Name (S/N)</div>
+                      <div>Current percentage: 50%</div>
+                      <div>Started at: 1/6/2023, 2pm</div>
+                      <div>Time elapsed: 50 mins</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Three boxes - charging history, favourites, map */}
+          <div className="container mx-auto px-4">    
             <div className="flex flex-wrap">
-              <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
+              <div className="pt-6 w-full md:w-4/12 px-4 text-center">
                 <Link to="/ChargingHistory" className="relative flex flex-col min-w-0 break-words bg-white hover:bg-blue-50 cursor-pointer w-full mb-8 shadow-lg rounded-lg">
                   <div className="px-4 py-5 flex-auto">
                     <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-400">
@@ -67,20 +92,20 @@ export default function Dashboard() {
                 </Link>
               </div>
 
-              <div className="pt-6 w-full md:w-4/12 px-4 text-center">
+              <div className="md:pt-6 w-full md:w-4/12 px-4 text-center">
                 <Link to="/Recommendations" className="relative flex flex-col min-w-0 break-words bg-white hover:bg-green-50 cursor-pointer w-full mb-8 shadow-lg rounded-lg">
-                    <div className="px-4 py-5 flex-auto">
-                      <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400">
-                        <i className="fas fa-map-marker-alt"></i>
-                      </div>
-                      <h6 className="text-xl font-semibold">
-                        Get recommendations on the go
-                      </h6>
-                      <p className="mt-2 mb-4 text-gray-600">
-                        Don't know where to find chargers? Use our map,<br></br>complete with recommendations for you!
-                      </p>
+                  <div className="px-4 py-5 flex-auto">
+                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400">
+                      <i className="fas fa-map-marker-alt"></i>
                     </div>
-                  
+                    <h6 className="text-xl font-semibold">
+                      Get recommendations on the go
+                    </h6>
+                    <p className="mt-2 mb-4 text-gray-600">
+                      Don't know where to find chargers? Use our map,<br></br>complete with recommendations for you!
+                    </p>
+                  </div>
+
                 </Link>
               </div>
 
